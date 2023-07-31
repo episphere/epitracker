c7d0b8cce34851b9774d069c3eb236ca6d4356b9
Sbehpour@deloitte.com <Sbehpour@deloitte.com>
2023-07-31 11:53:49 -0500

    removing extra immages

---
+++ src/pages/instruction.js
@@ -0,0 +1,29 @@
+
+//   export const renderInstructionPage = async () => {
+//     let template = `
+//     <div class="main-summary-row">
+//     <div class="align-left">
+//          <h1 class="page-header">Information on the Epitracker platform</h1>
+   
+//         <div class="home-page-stats font-size-18">
+//            <div class="main-summary-row">
+//                <div class="col align-left">
+//                   </br>
+                  
+//                   <b>Goal:</b>
+                  
+//                   Developing an epidemiology data tracker platform with real-time interactive visual analytics. <br>  
+                
+//                 <b>Data:</b>
+//                   The data comes from CBC national center of statistics for national health certificate data, CDC Wonder, a freely available platform where you can explore national health certificate data. 
+                    
+                
+//                 </div>
+//             </div>
+//         </div>     
+                  
+//           <div class="align-left" id="confluenceDataSummary"></div>
+//     </div>
+//   `;
+//     document.getElementById("instructionPage").innerHTML = template;
+//   };
