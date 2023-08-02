1f9391007ddc182d6f46e5298a0a49e4dd67bba6
Sbehpour@deloitte.com <Sbehpour@deloitte.com>
2023-08-01 19:33:07 -0500

    commenting

--- src/components/visualization/settingsVisualization.js
+++ src/components/visualization/settingsVisualization.js
@@ -1,17 +1,19 @@
+// An export statement defining a constant object called SETTINGS that contains various properties related to settings, each with a corresponding string value.
 export const SETTINGS = {
   GRAPH_TYPE: 'graph-type',
   SHOW_HIDE_TABLE: 'show-hide-table',
   START_ZERO: 'start-zero',
   COLOR_SCHEME: 'RdYlBu',
-}
-
+};
+// defining a constant object, SETTINGS_CONTENTS that uses computed property names to map each property from SETTINGS to an object with a 'render' property, which contains a function.
 const SETTINGS_CONTENTS = {
-  [SETTINGS.GRAPH_TYPE]: {render: getGraphType},
-  [SETTINGS.SHOW_HIDE_TABLE]: {render: getShowHideTable},
-  [SETTINGS.START_ZERO]: {render: getStartZero},
-  [SETTINGS.COLOR_SCHEME]: {render: getColorScheme}
-}
+  [SETTINGS.GRAPH_TYPE]: { render: getGraphType },
+  [SETTINGS.SHOW_HIDE_TABLE]: { render: getShowHideTable },
+  [SETTINGS.START_ZERO]: { render: getStartZero },
+  [SETTINGS.COLOR_SCHEME]: { render: getColorScheme },
+};
 
+//The function getGraphType generates and returns an HTML string representing a form-check input with a checkbox and label for 'Show Lines' in the quantile page. 
 function getGraphType() {
   return `
     <div class="form-check">
@@ -22,7 +24,7 @@ function getGraphType() {
     </div>
   `
 }
-
+//The function getShowHideTable()generates and returns an HTML string representing a form-check input with a checkbox and label for 'Show table' in both quantile and map pages.
 function getShowHideTable() {
   return `
     <div class="form-check">
@@ -33,6 +35,7 @@ function getShowHideTable() {
     </div>
   `
 }
+//The function getColorScheme() generates and returns an HTML string representing a form-selection input with selection choices and label for 'Scheme select' in both quantile and map pages.
 
 function getColorScheme() {
   return `
@@ -44,7 +47,7 @@ function getColorScheme() {
   </div>
   `
 }
-
+//The function getStartZero() generates and returns an HTML string representing a form-check input with a checkbox and label for 'start zero' on the quantile graph.
 function getStartZero() {
   return `
     <div class="form-check">
@@ -55,7 +58,8 @@ function getStartZero() {
     </div>
   `
 }
-
+// This function takes an optional array of setting names as 'settings'.
+// It generates an HTML string representing a visualization of the settings provided in the 'settings' array.
 export function renderSettingsVisualization(settings = []) {
   let template = `
     <div class="card rounded-1 mt-3">
