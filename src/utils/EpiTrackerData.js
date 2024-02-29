8a814af2854d9f15071f7e5460ec841e441bb50c
Sbehpour@deloitte.com <Sbehpour@deloitte.com>
2024-02-28 20:10:10 -0600

    prettier

--- src/utils/EpiTrackerData.js
+++ src/utils/EpiTrackerData.js
@@ -1,165 +1,168 @@
-import jszip from 'https://cdn.jsdelivr.net/npm/jszip@3.10.1/+esm'
-import * as aq from 'https://cdn.jsdelivr.net/npm/arquero@5.2.0/+esm'
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3-dsv@3.0.1/+esm'
+import jszip from "https://cdn.jsdelivr.net/npm/jszip@3.10.1/+esm";
+import * as aq from "https://cdn.jsdelivr.net/npm/arquero@5.2.0/+esm";
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3-dsv@3.0.1/+esm";
 
 export class EpiTrackerData {
-  constructor(args={}) {
+  constructor(args = {}) {
     this.args = {
       keepInMemory: "zipped", // "zipped", "unzipped", or "none",
-      postProcessCountyMortalityData: table => table, // Function takes an arquero table
-      ...args 
-    }
+      postProcessCountyMortalityData: (table) => table, // Function takes an arquero table
+      ...args,
+    };
 
-    this.dataMemoryMap = new Map()
-    this.postProcessCountyMortalityData = this.args.postProcessCountyMortalityData
+    this.dataMemoryMap = new Map();
+    this.postProcessCountyMortalityData =
+      this.args.postProcessCountyMortalityData;
   }
 
   async getQuantileMortalityData(query, args) {
     args = {
-      includeTotals: true, 
+      includeTotals: true,
       includeReduntantFields: true, // TODO: Implement
-      ...args 
-    }
+      ...args,
+    };
 
     query = {
       sex: "*",
-      race: "*", 
+      race: "*",
       cause: "*",
       year: "2020",
       quantile_field: "*",
-     // num_quantiles: "*", // TODO: Support this when needed
-      ...query
-    }
+      // num_quantiles: "*", // TODO: Support this when needed
+      ...query,
+    };
 
-    const year = query.year
-    delete query.year 
+    const year = query.year;
+    delete query.year;
 
-    let quantileMortalityData = (await this.#loadQuantileMortalityData(year))
-      .rename( {
-        race_ethnicity: "race",
-      })
+    let quantileMortalityData = (
+      await this.#loadQuantileMortalityData(year)
+    ).rename({
+      race_ethnicity: "race",
+    });
 
     // TODO: Fix this to allow all states
-    const aqFilter = []
-    for (const [k,v] of Object.entries(query)) {
+    const aqFilter = [];
+    for (const [k, v] of Object.entries(query)) {
       if (v != "*") {
-        aqFilter.push(`row.${k} == "${v}"`)
+        aqFilter.push(`row.${k} == "${v}"`);
       } else if (!args.includeTotals) {
-        aqFilter.push(`row.${k} != "All"`)
+        aqFilter.push(`row.${k} != "All"`);
       }
     }
 
-    const filterString = aqFilter.length > 0 ? `row => ` + aqFilter.join(" && ") : "row => true"
-    
-    return this.postProcessCountyMortalityData(quantileMortalityData
-      .filter(filterString)
-      .derive({
-        deaths: d => d.aq.op.parse_float(d.count),
-        population: d => d.aq.op.parse_float(d.population),
-        crude_rate: d => aq.op.parse_float(d.crude_rate),
-        age_adjusted_rate: d => aq.op.parse_float(d.age_adjusted_rate)
-      }))
-      
-      .objects()
+    const filterString =
+      aqFilter.length > 0 ? `row => ` + aqFilter.join(" && ") : "row => true";
+
+    return this.postProcessCountyMortalityData(
+      quantileMortalityData.filter(filterString).derive({
+        deaths: (d) => d.aq.op.parse_float(d.count),
+        population: (d) => d.aq.op.parse_float(d.population),
+        crude_rate: (d) => aq.op.parse_float(d.crude_rate),
+        age_adjusted_rate: (d) => aq.op.parse_float(d.age_adjusted_rate),
+      })
+    ).objects();
   }
 
   // TODO: Support multiple selection queries
-  async getCountyMortalityData(query, args={}) {
+  async getCountyMortalityData(query, args = {}) {
     args = {
-      includeTotals: true, 
+      includeTotals: true,
       includeReduntantFields: true, // TODO: Implement
-      ...args 
-    }
+      ...args,
+    };
 
     query = {
       sex: "*",
-      race: "*", 
+      race: "*",
       state_fips: "*",
       county_fips: "*",
       cause: "*",
       year: "2020",
-      ...query
-    }
+      ...query,
+    };
 
-    const year = query.year
-    delete query.year 
+    const year = query.year;
+    delete query.year;
 
-    let countyMortalityData = await this.#loadCountyMortalityData(year) 
+    let countyMortalityData = await this.#loadCountyMortalityData(year);
 
     // TODO: Fix this to allow all states
-    const aqFilter = []
-    for (const [k,v] of Object.entries(query)) {
+    const aqFilter = [];
+    for (const [k, v] of Object.entries(query)) {
       if (v != "*") {
-        aqFilter.push(`row.${k} == "${v}"`)
+        aqFilter.push(`row.${k} == "${v}"`);
       } else if (!args.includeTotals) {
-        aqFilter.push(`row.${k} != "All"`)
+        aqFilter.push(`row.${k} != "All"`);
       }
     }
 
-    const filterString = aqFilter.length > 0 ? `row => ` + aqFilter.join(" && ") : "row => true"
-    
-    return this.postProcessCountyMortalityData(countyMortalityData
-      .filter(filterString)
-      .derive({
-        deaths: d => d.aq.op.parse_float(d.deaths),
-        population: d => d.aq.op.parse_float(d.population),
-        crude_rate: d => aq.op.parse_float(d.crude_rate),
-        age_adjusted_rate: d => aq.op.parse_float(d.age_adjusted_rate)
-      }))
-      .objects()
+    const filterString =
+      aqFilter.length > 0 ? `row => ` + aqFilter.join(" && ") : "row => true";
+
+    return this.postProcessCountyMortalityData(
+      countyMortalityData.filter(filterString).derive({
+        deaths: (d) => d.aq.op.parse_float(d.deaths),
+        population: (d) => d.aq.op.parse_float(d.population),
+        crude_rate: (d) => aq.op.parse_float(d.crude_rate),
+        age_adjusted_rate: (d) => aq.op.parse_float(d.age_adjusted_rate),
+      })
+    ).objects();
   }
 
-  async getDemographicMortalityData(query, args={}) {
-
+  async getDemographicMortalityData(query, args = {}) {
     args = {
-      includeTotals: true, 
+      includeTotals: true,
       includeReduntantFields: true, // TODO: Implement
-      ...args 
-    }
+      ...args,
+    };
 
     query = {
       sex: "*",
-      race: "*", 
+      race: "*",
       state_fips: "*",
       cause: "*",
       year: "2020",
       age_group: "*",
-      ...query
-    }
+      ...query,
+    };
 
-    const year = query.year
-    delete query.year 
+    const year = query.year;
+    delete query.year;
 
-    let demographicData = await this.#loadDemographicData(year) 
+    let demographicData = await this.#loadDemographicData(year);
 
     // TODO: Fix this to allow all states
-    const aqFilter = []
-    for (const [k,v] of Object.entries(query)) {
+    const aqFilter = [];
+    for (const [k, v] of Object.entries(query)) {
       if (v != "*") {
-        aqFilter.push(`row.${k} == "${v}"`)
+        aqFilter.push(`row.${k} == "${v}"`);
       } else if (!args.includeTotals) {
-        aqFilter.push(`row.${k} != "All"`)
+        aqFilter.push(`row.${k} != "All"`);
       }
     }
 
-    const filterString = aqFilter.length > 0 ? `row => ` + aqFilter.join(" && ") : "row => true"
-    
+    const filterString =
+      aqFilter.length > 0 ? `row => ` + aqFilter.join(" && ") : "row => true";
+
     return demographicData
       .filter(filterString)
       .derive({
-        deaths: d => d.aq.op.parse_float(d.deaths),
-        population: d => d.aq.op.parse_float(d.population),
-        crude_rate: d => aq.op.parse_float(d.crude_rate),
-        age_adjusted_rate: d => aq.op.parse_float(d.age_adjusted_rate)
+        deaths: (d) => d.aq.op.parse_float(d.deaths),
+        population: (d) => d.aq.op.parse_float(d.population),
+        crude_rate: (d) => aq.op.parse_float(d.crude_rate),
+        age_adjusted_rate: (d) => aq.op.parse_float(d.age_adjusted_rate),
       })
-      .objects()
+      .objects();
   }
-
 
   async #loadDemographicData(year) {
     // Year currently takes "2018", "2019", "2020", and "2018-2020"
 
-    return this.#smartLoadZipData(`../data/demographic/demographic_data_${year}.csv.zip`, `demographic_data_${year}.csv`)
+    return this.#smartLoadZipData(
+      `../data/demographic/demographic_data_${year}.csv.zip`,
+      `demographic_data_${year}.csv`
+    );
   }
 
   async #loadCountyMortalityData(year) {
@@ -173,7 +176,7 @@ export class EpiTrackerData {
     //   if (this.args.keepInMemory == "zipped") {
     //     this.dataMemoryMap.set(`mortality-data-${year}`, data)
     //   }
-    // } 
+    // }
 
     // if (data instanceof Blob) {
     //   const zip = new jszip()
@@ -186,7 +189,10 @@ export class EpiTrackerData {
     //   this.dataMemoryMap.set(`mortality-data-${year}`, data)
     // }
 
-    return this.#smartLoadZipData(`../data/mortality/age_adjusted_data_${year}.csv.zip`, `age_adjusted_data_${year}.csv`)
+    return this.#smartLoadZipData(
+      `../data/mortality/age_adjusted_data_${year}.csv.zip`,
+      `age_adjusted_data_${year}.csv`
+    );
   }
 
   async #loadQuantileMortalityData(year) {
@@ -200,7 +206,7 @@ export class EpiTrackerData {
     //   if (this.args.keepInMemory == "zipped") {
     //     this.dataMemoryMap.set(`quantile-data-${year}`, data)
     //   }
-    // } 
+    // }
 
     // if (data instanceof Blob) {
     //   const zip = new jszip()
@@ -215,31 +221,33 @@ export class EpiTrackerData {
 
     // return data
 
-    return this.#smartLoadZipData(`../data/quantile/quantile_data_${year}.csv.zip`, `quantile_data_${year}.csv`)
+    return this.#smartLoadZipData(
+      `../data/quantile/quantile_data_${year}.csv.zip`,
+      `quantile_data_${year}.csv`
+    );
   }
 
   async #smartLoadZipData(url, filename) {
-    let data
+    let data;
     if (!(data = this.dataMemoryMap.get(url))) {
-      data = await (await fetch(url)).blob()
+      data = await (await fetch(url)).blob();
 
       if (this.args.keepInMemory == "zipped") {
-        this.dataMemoryMap.set(url, data)
+        this.dataMemoryMap.set(url, data);
       }
-    } 
+    }
 
     if (data instanceof Blob) {
-      const zip = new jszip()
-      await zip.loadAsync(data)
-      data = await d3.csvParse((await zip.file(filename).async("string")))
+      const zip = new jszip();
+      await zip.loadAsync(data);
+      data = await d3.csvParse(await zip.file(filename).async("string"));
     }
 
-    data = aq.from(data)
+    data = aq.from(data);
     if (this.args.keepInMemory == "unzipped") {
-      this.dataMemoryMap.set(url, data)
+      this.dataMemoryMap.set(url, data);
     }
 
-    return data
+    return data;
   }
-
 }
--- src/utils/input2.js
+++ src/utils/input2.js
@@ -1,23 +1,23 @@
 // export function hookSelect(select) {
-  
+
 //   function setOptions(options) {
 //     select.innerHTML = ``
 //     for (let option of options) {
 //       if (typeof option == "string") {
 //         option = {label: option, value: option}
-//       } 
+//       }
 
 //       if (option.label == null) {
 //         option.label = option.value
 //       }
-      
+
 //       const optionElement = document.createElement("option")
-//       optionElement.value = option.value 
-//       optionElement.innerHTML = option.label 
+//       optionElement.value = option.value
+//       optionElement.innerHTML = option.label
 //       if (option.selected) {
 //         optionElement.setAttribute("selected", "")
 //       }
-      
+
 //       select.appendChild(optionElement)
 //     }
 //   }
@@ -38,112 +38,130 @@
 // }
 
 export function hookCheckbox(selector, state, valueProperty) {
-  const check = document.querySelector(selector)
+  const check = document.querySelector(selector);
   if (check == null) {
-    throw new Error(`No element found for ${selector}`)
+    throw new Error(`No element found for ${selector}`);
   }
 
   function setEnabled(enabled) {
     if (enabled) {
-      check.setAttribute("checked", "")
+      check.setAttribute("checked", "");
     } else {
-      check.removeAttribute("checked")
+      check.removeAttribute("checked");
     }
   }
 
-  state.subscribe(valueProperty, () => setEnabled(state[valueProperty]))
-  check.addEventListener("click", () => state[valueProperty] = check.checked)
+  state.subscribe(valueProperty, () => setEnabled(state[valueProperty]));
+  check.addEventListener("click", () => (state[valueProperty] = check.checked));
 
-  setEnabled(state[valueProperty])
+  setEnabled(state[valueProperty]);
 }
 
-export function hookSelectChoices(selector, state, valueProperty, optionsProperty, format=d => d, searchEnabled=false,
-  sorter=(a,b)=>a-b) {
-  const select = document.querySelector(selector)
+export function hookSelectChoices(
+  selector,
+  state,
+  valueProperty,
+  optionsProperty,
+  format = (d) => d,
+  searchEnabled = false,
+  sorter = (a, b) => a - b
+) {
+  const select = document.querySelector(selector);
   if (select == null) {
-    throw new Error(`No element found for ${selector}`)
+    throw new Error(`No element found for ${selector}`);
   }
 
-  const choices = new Choices(select, {allowHTML: false, itemSelectText: "", searchEnabled, sorter})
+  const choices = new Choices(select, {
+    allowHTML: false,
+    itemSelectText: "",
+    searchEnabled,
+    sorter,
+  });
 
   function setOptions() {
     //console.log(selector, state[optionsProperty])
     if (state[optionsProperty]) {
-      const options = state[optionsProperty].map(d => typeof d == "string" ? {label: d, value: d} : d)
-      choices.setChoices(options, "value", "label", true)
+      const options = state[optionsProperty].map((d) =>
+        typeof d == "string" ? { label: d, value: d } : d
+      );
+      choices.setChoices(options, "value", "label", true);
       if (state[valueProperty]) {
-        choices.setChoiceByValue([state[valueProperty]])
+        choices.setChoiceByValue([state[valueProperty]]);
       }
     }
   }
 
   state.subscribe(optionsProperty, () => {
-    setOptions()
-  })
+    setOptions();
+  });
 
-  state.subscribe(valueProperty, () => {   
-    choices.setChoiceByValue([state[valueProperty]])
-  })
+  state.subscribe(valueProperty, () => {
+    choices.setChoiceByValue([state[valueProperty]]);
+  });
 
   select.addEventListener("change", () => {
-    state[valueProperty] = select.value 
-  })
+    state[valueProperty] = select.value;
+  });
 
-  setOptions()
+  setOptions();
 
-  return choices
+  return choices;
 }
 
-export function hookSelect(selector, state, valueProperty, optionsProperty, format = d => d) {
-  
-  const select = document.querySelector(selector)
+export function hookSelect(
+  selector,
+  state,
+  valueProperty,
+  optionsProperty,
+  format = (d) => d
+) {
+  const select = document.querySelector(selector);
   if (select == null) {
-    throw new Error(`No element found for ${selector}`)
+    throw new Error(`No element found for ${selector}`);
   }
 
   function setOptions(options) {
-
-    const selectOptions = []
-    select.innerHTML = ``
+    const selectOptions = [];
+    select.innerHTML = ``;
 
     if (options) {
       for (let option of options) {
         if (typeof option == "string") {
-          option = { value: option, label: format(option) }
+          option = { value: option, label: format(option) };
         }
-        selectOptions.push(option)
-        const optionElement = document.createElement("option")
-        optionElement.value = option.value
-        optionElement.innerText = option.label
-        
+        selectOptions.push(option);
+        const optionElement = document.createElement("option");
+        optionElement.value = option.value;
+        optionElement.innerText = option.label;
+
         if (option.value == state[valueProperty]) {
-          optionElement.selected = true
-          select.value = option.value
+          optionElement.selected = true;
+          select.value = option.value;
         }
-        select.appendChild(optionElement)
+        select.appendChild(optionElement);
       }
     }
   }
 
   state.subscribe(optionsProperty, () => {
-    setOptions(state[optionsProperty])
-    state[valueProperty] = select.value
-  })
+    setOptions(state[optionsProperty]);
+    state[valueProperty] = select.value;
+  });
 
-  state.subscribe(valueProperty, () => {   
+  state.subscribe(valueProperty, () => {
     for (const option of select.options) {
       if (option.value == state[valueProperty]) {
-        option.selected = true 
+        option.selected = true;
       } else {
-        option.selected = false
+        option.selected = false;
       }
     }
-  })
+  });
 
   select.addEventListener("change", () => {
-    state[valueProperty] = select.value 
-  })
-  setOptions(state[optionsProperty])
+    state[valueProperty] = select.value;
+  });
+  setOptions(state[optionsProperty]);
 
   // if (select.value != "") {
   //   state[valueProperty] = select.value
@@ -152,8 +170,8 @@ export function hookSelect(selector, state, valueProperty, optionsProperty, form
 
 export function setDisabled(element, disabled) {
   if (disabled) {
-    element.setAttribute("disabled", "")
+    element.setAttribute("disabled", "");
   } else {
-    element.removeAttribute("disabled")
+    element.removeAttribute("disabled");
   }
 }
