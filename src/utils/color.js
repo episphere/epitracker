ea9b456055f07b16489b2163944a1d99817c257f
Sbehpour@deloitte.com <Sbehpour@deloitte.com>
2024-04-17 15:44:11 -0500

    Add color json file

---
+++ src/utils/color.js
@@ -0,0 +1,28 @@
+export const COLORS = {
+  sex: {
+    'Male': '#f00',
+    'Female': '#0f0'
+  },
+  race: {
+    "All": "#000",
+    "Non-Hispanic Black or African American": '#ff0',
+    "Non-Hispanic American Indian or Alaska Native": "#b9936c",
+    "Non-Hispanic White": "#e6e2d3",
+    "Non-Hispanic Asian": "##d6cbd3",
+    "Hispanic": "#f0f",
+    "Non-Hispanic More than one race": "#563f46",
+    "Non-Hispanic Native Hawaiian or Other Pacific Islander": "#0ffk000"
+  },
+  age_group: {
+    "85+": "#555",
+    "75-84": "#f0f0f0",
+    "65-74": "#ff00f0",
+    "55-64": "#0ff",
+    "45-54": "#618685",
+    "35-44": "#50394c",
+    "25-34": "#ffef96",
+    "15-24": "#c1946a",
+    "5-14": "#00f",
+    "0-4": "#0001fkl"
+  }
+}
